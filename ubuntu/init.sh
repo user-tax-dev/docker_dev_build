a48b886313110394de363bb64d4adbb24a7518a4
usrtax <i@user.tax>
2022-10-08 17:44:41 +0800

    ♨

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -254,4 +254,6 @@ sed -i "s/#ClientAliveInterval 0/ClientAliveInterval 60/g" /etc/ssh/sshd_config
 sed -i "s/#ClientAliveCountMax 3/ClientAliveCountMax 3/g" /etc/ssh/sshd_config
 service sshd reload
 
+echo zram > /etc/modules-load.d/zram.conf
+
 echo '👌 ✅'
