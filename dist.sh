3f14f1c5c7ffd144b54bd4106e7de8dc438ab045
usrtax <i@user.tax>
2022-10-18 13:29:27 +0800

    ♨

--- dist.sh
+++ dist.sh
@@ -5,13 +5,10 @@ cd $DIR
 
 org=iuser
 
-
-for os in $(find ./build/* -maxdepth 0 -type d)
-do
+for os in $(find ./build/* -maxdepth 0 -type d); do
   os=$(basename $os)
   echo $os
   sh -c "./build.sh $org $os && exec docker push -a $org/dev_$os" &
 done
 
 wait
-
--- ubuntu/boot.sh
+++ ubuntu/boot.sh
@@ -6,19 +6,19 @@ apt-get update
 apt-get install -y curl git
 cd ~
 
-curl --connect-timeout 2 -m 4 -s https://t.co > /dev/null || CN=1
+curl --connect-timeout 2 -m 4 -s https://t.co >/dev/null || CN=1
 [ $CN ] && git config --global url."https://ghproxy.com/https://github.com".insteadOf "https://github.com"
 
-clone(){
-out=$(basename $1)
-if [ ! -d "$out" ]; then
-git clone --recursive --depth=1 https://github.com/$1.git
-else
-cd $out
-git fetch --all
-git reset --hard origin/main
-cd $DIR
-fi
+clone() {
+  out=$(basename $1)
+  if [ ! -d "$out" ]; then
+    git clone --recursive --depth=1 https://github.com/$1.git
+  else
+    cd $out
+    git fetch --all
+    git reset --hard origin/main
+    cd $DIR
+  fi
 }
 
 clone user-tax-dev/docker_dev_build
